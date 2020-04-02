656b50b49466aef3ea922b33e931c4a3e24fc8a7
Alexander Paz <alexjpaz@gmail.com>
2020-04-02 16:24:19 -0400

    Adding correct layout to script

---
+++ _scripts/convert.py
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x
+
+input=$1
+basename=$(basename ${input})
+name=${basename/\.*/}
+suffix=${basename/*\./}
+today=$(date "+%Y-%m-%d")
+
+output=$(cat ${input} | openssl base64 | tr -d '\n')
+
+cat << EOF > ./_posts/${today}-${name}.md
+---
+layout: post-art
+category: scene
+title: ${name}
+image: data:image/${suffix};base64,${output}
+---
+EOF
