e9a53f414db84f6e5b1c53be23e9e79fefe214cf
Alexander Paz <alexjpaz@gmail.com>
2020-04-03 13:07:55 -0400

    fixing the convert script to not include category

--- _scripts/convert.py
+++ _scripts/convert.py
@@ -12,7 +12,6 @@ output=$(cat ${input} | openssl base64 | tr -d '\n')
 cat << EOF > ./_posts/${today}-${name}.md
 ---
 layout: post-art
-category: scene
 title: ${name}
 image: data:image/${suffix};base64,${output}
 ---
