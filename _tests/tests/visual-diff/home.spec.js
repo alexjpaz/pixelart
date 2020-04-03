99fa53088ea55249f76f8139fe6fd19a47c0afcb
Alexander Paz <alexjpaz@gmail.com>
2020-04-03 13:11:14 -0400

    Adding melon post

--- _tests/tests/visual-diff/home.spec.js
+++ _tests/tests/visual-diff/home.spec.js
@@ -7,7 +7,7 @@ afterAll(async () => await environment.teardown());
 
 environment.navigateAndCapture('test', '/test');
 environment.navigateAndCapture('home', '/pixelart');
-environment.navigateAndCapture('blog-post', '/pixelart/2020/04/01/midnight-phase.html');
+environment.navigateAndCapture('blog-post', '/pixelart/2020/04/03/melon.html');
 environment.navigateAndCapture('tags', '/pixelart/tags?tag=pixel');
 environment.navigateAndCapture('404', '/pixelart/404');
 environment.navigateAndCapture('gallery', '/pixelart/gallery');
