3c2be854150d57c84872e9491cbb6e9338182a4d
Alexander Paz <alexjpaz@gmail.com>
2020-04-11 19:37:55 -0400

    Fixing script to add art category

--- _scripts/convert.py
+++ _scripts/convert.py
@@ -9,10 +9,13 @@ today=$(date "+%Y-%m-%d")
 
 output=$(cat ${input} | openssl base64 | tr -d '\n')
 
-cat << EOF > ./_posts/${today}-${name}.md
+mkdir -p ./_posts/art/
+
+cat << EOF > ./_posts/art/${today}-${name}.md
 ---
 layout: post-art
 title: ${name}
+category: art
 image: data:image/${suffix};base64,${output}
 ---
 EOF
