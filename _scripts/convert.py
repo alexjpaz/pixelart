d5aa76d8cf5f2577e50e75db8f0ed96aa90d224c
Alexander Paz <alexjpaz@gmail.com>
2020-04-20 19:22:56 -0400

    Fixing script

--- _scripts/convert.py
+++ _scripts/convert.py
@@ -6,7 +6,7 @@ basename=$(basename ${input})
 name=${basename/\.*/}
 suffix=${basename/*\./}
 today=$(date "+%Y-%m-%d")
-post=./_posts/arg/${today}-${name}.md
+post=./_posts/art/${today}-${name}.md
 
 mkdir -p ./_posts/art/
 cp $input ./assets/art/${name}.${suffix}
